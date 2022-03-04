02778d24b40c87a07b5c857979d46c6a32257a63
Hoan9696 <phanhoan.vnu@gmail.com>
2022-03-04 09:02:02 +0700

    first commit

---
+++ src/main/java/com/springbootjwtcmc/SpringBootJwtCmcApplication.java
@@ -0,0 +1,13 @@
+package com.springbootjwtcmc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJwtCmcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJwtCmcApplication.class, args);
+	}
+
+}
