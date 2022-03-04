89b89ce0330a15a8e7b765c0b473e7e031416f9a
Hoan9696 <phanhoan.vnu@gmail.com>
2022-03-04 14:24:46 +0700

    commit code jwt

--- src/test/java/com/example/spring_jwt_cmc/SpringJwtCmcApplicationTests.java
+++ src/test/java/com/example/spring_jwt_cmc/SpringJwtCmcApplicationTests.java
@@ -1,10 +1,10 @@
-package com.springbootjwtcmc;
+package com.example.spring_jwt_cmc;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringBootJwtCmcApplicationTests {
+class SpringJwtCmcApplicationTests {
 
 	@Test
 	void contextLoads() {
